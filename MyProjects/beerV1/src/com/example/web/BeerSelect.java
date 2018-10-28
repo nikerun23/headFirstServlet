5a7f6f12d3339e25c1cba6ed97ee8580f3c848ce
HK Lee <nikerun23@naver.com>
2018-10-28 20:30:11 +0900

    Response setContentType MIME

--- MyProjects/beerV1/src/com/example/web/BeerSelect.java
+++ MyProjects/beerV1/src/com/example/web/BeerSelect.java
@@ -32,4 +32,22 @@ public class BeerSelect extends HttpServlet {
     view.forward(request, response);
   }
 
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    response.setContentType("application/txt");
+
+    ServletContext ctx = getServletContext();
+    InputStream is = ctx.getResourceAsStream("/test.txt");
+
+    int read = 0;
+    byte[] bytes = new byte[1024];
+
+    OutputStream os = response.getOutputStream();
+    while((read = is.read(bytes)) != -1) {
+      os.write(bytes, 0, read);
+    }
+    os.flush();
+    os.close();
+  }
+
 }
