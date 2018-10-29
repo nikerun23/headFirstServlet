fc3d5c670abf883b1f86bdfc0a1f230e9094bc10
HK Lee <nikerun23@naver.com>
2018-10-29 15:32:22 +0900

    ServletContext Event Listener

--- MyProjects/beerV1/src/com/example/MyServletContextListener.java
+++ MyProjects/beerV1/src/com/example/MyServletContextListener.java
@@ -1,13 +1,21 @@
+package com.example;
+
 import javax.servlet.*;
+import com.example.Dog;
 
 public class MyServletContextListener implements ServletContextListener {
 
   public void contextInitialized(ServletContextEvent event) {
-    // DataSource 생성
+
+    ServletContext sc = event.getServletContext();
+    String dogBreed = sc.getInitParameter("breed");
+    Dog dog = new Dog(dogBreed);
+    sc.setAttribute("dog", dog);
+
   }
 
   public void contextDestroyed(ServletContextEvent event) {
-    // DataSource 소멸
+
   }
 
 }
