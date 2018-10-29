44f56fdc07fdcca935bea985fa7429260822ae37
HK Lee <nikerun23@naver.com>
2018-10-29 15:33:11 +0900

    Dog Object

---
+++ MyProjects/beerV1/src/com/example/Dog.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public Dog class {
+
+  private String breed;
+
+  public Dog(String breed) {
+    this.breed = breed;
+  }
+
+  public String getBreed(){
+    return this.breed;
+  }
+
+}
